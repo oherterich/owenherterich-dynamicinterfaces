ea06ca7736d4d0f55fc1003a76d7fbd5e3d9ff28
Owen Herterich <oherterich@gmail.com>
2014-02-07 16:57:19 -0500

    added stuff and pics pages

---
+++ assignment_1998/code/pics.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HI, MY NAME IS OWEN!!!</title>
+
+		<link rel="stylesheet" href="css/style.css">
+
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<section id="content">
+			<header>
+				<h1>STUFF I LIKE</h1>
+				<h2>CHECK IT OUT!</h2>
+			</header>
+			<article id="stuff">
+				<p>
+
+				</p>
+			</article>
+			<?php
+				require("nav.html");
+				require("footer.html");
+			?>
+		</section>
+	</body>
+</html>
---
+++ assignment_1998/code/stuff.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HI, MY NAME IS OWEN!!!</title>
+
+		<link rel="stylesheet" href="css/style.css">
+
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<section id="content">
+			<header>
+				<h1>STUFF I LIKE</h1>
+				<h2>CHECK IT OUT!</h2>
+			</header>
+			<article id="stuff">
+				<p>
+					<img src="img/greenday.jpg">
+					<img src="img/discman.gif">
+					<img src="img/simpsons.jpg">
+					<img src="img/beach.gif">
+					<img src="img/titanic.gif">
+					<img src="img/jordan.gif">
+					<img src="img/wwf.gif">
+					<img src="img/tonyhawk.gif">
+					<img src="img/finalfantasy.jpg">
+					<img src="img/matrix.jpg">
+					<img src="img/blink.gif">
+					<img src="img/ferrari.gif">
+				</p>
+			</article>
+			<?php
+				require("nav.html");
+				require("footer.html");
+			?>
+		</section>
+	</body>
+</html>
