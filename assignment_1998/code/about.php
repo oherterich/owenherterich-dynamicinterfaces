cadc14ade7441701962c312f9bb915901009ffa3
Owen Herterich <oherterich@gmail.com>
2014-02-07 16:28:01 -0500

    initial home page and about page

---
+++ assignment_1998/code/about.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HI, MY NAME IS OWEN!!!</title>
+
+		<link rel="stylesheet" href="css/style.css">
+
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<section id="content">
+			<header>
+				<h1>ABOUT ME</h1>
+				<p><img src="img/pizza.gif"></p>
+			</header>
+			<article id="aboutme">
+				<p>WELL I DON'T REALLY KNO WAT TO SAY! I'M OWEN, I'M JUST A REGULAR GUY LIVING LIFE HAHA</p>
+				<p>WELL I'M FROM TEXAS (NOT THE DESERT PART THO HAHA) BUT NOW I LIVE IN NEW YORK. THIS CITY IS SO BIG! MY FAVORITE THINGS TO DO ARE PLAY THE DRUMS AND WATCH MOVIES. DON'T ASK ME WHAT MY FAVORITE MOVIE IS THO, BECAUSE I DON'T KNOW HAHA. UMMM WHAT ELSE WELL I'M LEFT HANDED ISN'T THAT CRAZY!!! MY FAVORITE FOOD IS DEFINITELY PIZZA, PROBABLY PIZZA HUT! WELL I THINK THAT'S IT! I'VE BEEN WORKING ON MAKING THIS WEBSITE FOR A WHILE, IT'S PRETTY HARD BUT I THINK I HAVE THE HANG OF IT???? YOU TELL ME HAHA!!!</p> 
+			</article>
+			<?php
+				require("nav.html");
+				require("footer.html");
+			?>
+		</section>
+	</body>
+</html>
---
+++ assignment_1998/code/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HI, MY NAME IS OWEN!!!</title>
+
+		<link rel="stylesheet" href="css/style.css">
+
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<section id="content">
+			<header>
+				<h1>HI, MY NAME IS OWEN!!!</h1>
+				<p><img src="img/me.gif"></p>
+			</header>
+			<article id="intro">
+				<h2>THANKS FOR STOPPING BY!</h2>
+				<p>CHECK OUT MY LINKS BELOW, SOME OF THEM ARE PRETTY INTERESTING HAHA</p>
+				<p><img src="img/spinningearth.gif"></img></p>
+			</article>
+			<?php
+				require("nav.html");
+				require("footer.html");
+			?>
+		</section>
+	</body>
+</html>
