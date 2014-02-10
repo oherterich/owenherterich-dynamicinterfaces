1b56fc60da3180cd16cdf97da9ef7af25aaebae5
Owen Herterich <oherterich@gmail.com>
2014-02-09 21:36:08 -0500

    finished guestbook, renamed links

--- assignment_1998/code/diary.php
+++ assignment_1998/code/diary.php
@@ -10,20 +10,12 @@
 	<body>
 		<section id="content">
 			<header>
-				<h1>MY DIARY</h1>
+				<h1>MY GUESTBOOK</h1>
 			</header>
-			<article id="diary">
-				<p>
-					<img src="img/construction-tape.gif">
-					<img src="img/construction.gif">
-					<img src="img/construction-tape.gif">
-				</p>
-			</article>
-
 			<article id="guestbook-form">
 				<form action="diary.php" method="post">
 					Name: <input type="text" name="name"> <br/>
-					Message: <textarea rows="4" cols="50" name="message"></textarea>
+					Message: <br/><textarea rows="4" cols="50" name="message"></textarea><br/>
 					<input type="submit">
 				</form>
 			</article>
@@ -47,15 +39,16 @@
 				$query = mysql_query($sql);
 
 				while ( $data = mysql_fetch_assoc($query) ) {
-					echo "<h2>" . $data['name'] . "</h2>";
 					echo "<h3>" . $data['message'] . "</h3>";
-					echo "<h4>" . $data['date'] . "</h4>";
+					echo "<h4>From <span id='name'>" . $data['name'] . "</span> on <span id='date'>" . $data['date'] . "</span></h4>";
+					echo "<br/>";
 				}
-
-				require("nav.html");
-				require("footer.html");
 			?>
 		</article>
+		<?php
+			require("nav.html");
+			require("footer.html");
+		?>
 		</section>
 	</body>
 </html>
--- assignment_1998/code/guestbook.php
+++ assignment_1998/code/guestbook.php
@@ -1,32 +1,54 @@
-<?php
-	if ( isset( $_POST['name'] ) && isset( $_POST['message'] ) ) {
-		$name = $_POST['name'];
-		$message = $_POST['message'];
-		$date = date('d-m-Y H:i:s');
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HI, MY NAME IS OWEN!!!</title>
 
-		$mysql = mysql_connect('127.0.0.1', 'root', 'root');
-		
-		mysql_select_db('ninetyeight', $mysql);
+		<link rel="stylesheet" href="css/style.css">
 
-		//First, let's post the most recent post into our guestbook.
-		$sql = "INSERT INTO Guestbook (name, message, date) VALUES ( '$name', '$message', '$date' )";
-		$query = mysql_query($sql);
+		<script src="js/script.js"></script>
+	</head>
+	<body>
+		<section id="content">
+			<header>
+				<h1>MY GUESTBOOK</h1>
+			</header>
+			<article id="guestbook-form">
+				<form action="diary.php" method="post">
+					Name: <input type="text" name="name"> <br/>
+					Message: <br/><textarea rows="4" cols="50" name="message"></textarea><br/>
+					<input type="submit">
+				</form>
+			</article>
+			<article id="guestbook">
+			<?php
+				$mysql = mysql_connect('127.0.0.1', 'root', 'root');	
+				mysql_select_db('ninetyeight', $mysql);
+				
+				if ( isset( $_POST['name'] ) && isset( $_POST['message'] ) ) {
+					$name = $_POST['name'];
+					$message = $_POST['message'];
+					$date = date('d-m-Y H:i:s');
 
+					//First, let's post the most recent post into our guestbook.
+					$sql = "INSERT INTO Guestbook (name, message, date) VALUES ( '$name', '$message', '$date' )";
+					$query = mysql_query($sql);
+				}
 
-		//Next, let's get all of the past posts in the guestbook.
-		$sql = "SELECT * FROM Guestbook";
-		$query = mysql_query($sql);
+				//Next, let's get all of the past posts in the guestbook.
+				$sql = "SELECT * FROM Guestbook";
+				$query = mysql_query($sql);
 
-		echo "<table>";
-		while ( $data = mysql_fetch_assoc($query) ) {
-			echo "<tr>";
-			echo "<td>" . $data['name'] . "</td>";
-			echo "<td>" . $data['message'] . "</td>";
-			echo "<td>" . $data['date'] . "</td>";
-			echo "</tr>";
-		}
-		echo "</table>";
-
-		//echo $message . " " . $name;
-	}
-?>
+				while ( $data = mysql_fetch_assoc($query) ) {
+					echo "<h3>" . $data['message'] . "</h3>";
+					echo "<h4>From <span id='name'>" . $data['name'] . "</span> on <span id='date'>" . $data['date'] . "</span></h4>";
+					echo "<br/>";
+				}
+			?>
+		</article>
+		<?php
+			require("nav.html");
+			require("footer.html");
+		?>
+		</section>
+	</body>
+</html>
